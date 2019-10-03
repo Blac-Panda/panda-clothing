c8ef3ed2f8415b3fe912c76a661a3ef30d708f52
Blac-Panda <oyefesotunmise@gmail.com>
2019-10-03 14:40:41 +0100

    Updates

---
+++ precache-manifest.b7ef278bbf1de330160dca3c3a9eca4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba90723fabbadfcac0fbc1068e6f662",
+    "url": "/panda-clothing/index.html"
+  },
+  {
+    "revision": "dae879c4aa6328aa6416",
+    "url": "/panda-clothing/static/css/main.ebaa5349.chunk.css"
+  },
+  {
+    "revision": "12cd60c0d23576cabf2e",
+    "url": "/panda-clothing/static/js/2.f8540ef2.chunk.js"
+  },
+  {
+    "revision": "dae879c4aa6328aa6416",
+    "url": "/panda-clothing/static/js/main.72ea1911.chunk.js"
+  },
+  {
+    "revision": "602ffeb5facf3a427c29",
+    "url": "/panda-clothing/static/js/runtime~main.811f1b0c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/panda-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/panda-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
